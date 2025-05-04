2ff0f9857ba65f929f402b07e0195092177c2305
nomadicvince <cosmiqwarrior@gmail.com>
2025-05-04 16:08:25 -0400

    Added Hello World

---
+++ systems/hello_world/hello_world/target/debug/deps/hello_world-2443d5adfb8dea4e.d
@@ -0,0 +1,5 @@
+/home/cosmiq/repos/rust_practice/systems/hello_world/hello_world/target/debug/deps/hello_world-2443d5adfb8dea4e: src/main.rs
+
+/home/cosmiq/repos/rust_practice/systems/hello_world/hello_world/target/debug/deps/hello_world-2443d5adfb8dea4e.d: src/main.rs
+
+src/main.rs:
---
+++ systems/hello_world/hello_world/target/debug/hello_world.d
@@ -0,0 +1,1 @@
+/home/cosmiq/repos/rust_practice/systems/hello_world/hello_world/target/debug/hello_world: /home/cosmiq/repos/rust_practice/systems/hello_world/hello_world/src/main.rs
